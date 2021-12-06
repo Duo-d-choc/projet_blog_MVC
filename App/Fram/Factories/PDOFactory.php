49408b12d082b7af500e5f78c541613153aa09ea
NicolasFOESSEL <nicofoessel91740@gmail.com>
2021-12-06 14:07:18 +0100

    ajout bdd
    ajout connexion bdd

---
+++ App/Fram/Factories/PDOFactory.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Fram\Factories;
+
+class PDOFactory
+{
+    private static string $dsn = 'mysql:host=localhost:3306';
+    private static string $userName = 'root';
+    private static string $password = '';
+
+    public static function getMySqlConnexion(): PDO{
+        return new PDO(self::$dsn, self::$userName, self::$password);
+    }
+}
--- App/Fram/Router.php
+++ App/Fram/Router.php
@@ -8,5 +8,7 @@ class Router
 {
     public function getController()
     {
+
+
     }
 }
