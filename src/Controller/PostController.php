c6d7bc8c7ba88f71ab95ebc9b357b1a0093f5075
NicolasFOESSEL <nicofoessel91740@gmail.com>
2021-12-06 11:15:42 +0100

    Update PostController.php

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -9,45 +9,5 @@ use App\Manager\PostManager;
 
 class PostController extends BaseController
 {
-    /**
-     * Show all Posts
-     */
-    public function executeIndex()
-    {
-        $postManager = new PostManager(PDOFactory::getMysqlConnection());
-        $posts = $postManager->getAllPosts();
 
-        $this->render(
-            'home.php',
-            [
-                'posts' => $posts,
-                'user' => new Author(),
-                'test' => 'je suis un test'
-            ],
-            'Home page'
-        );
-
-    }
-
-    public function executeShow()
-    {
-        Flash::setFlash('alert', 'je suis une alerte');
-
-        $this->render(
-            'show.php',
-            [
-                'test' => 'article ' . $this->params['id']
-            ],
-            'Show Page'
-        );
-    }
-
-    public function executeAuthor()
-    {
-        $this->render(
-            'author.php',
-            [],
-            'Auteur'
-        );
-    }
 }
