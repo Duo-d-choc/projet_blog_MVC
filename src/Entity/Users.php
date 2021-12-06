925f87249d747eebcfdfd1bdd1f50a169c8e8244
NicolasFOESSEL <nicofoessel91740@gmail.com>
2021-12-06 11:47:52 +0100

    Create Users.php

---
+++ src/Entity/Users.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Entity;
+
+class Users
+{
+
+    private int $id;
+    private string $name;
+    private string $statut;
+
+
+
+
+    /**
+     * @return int
+     */
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatut(): string
+    {
+        return $this->statut;
+    }
+
+    /**
+     * @param string $statut
+     */
+    public function setStatut(string $statut): void
+    {
+        $this->statut = $statut;
+    }
+
+
+
+    public function __construct($id, $name)
+    {
+
+    }
+}
