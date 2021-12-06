3574461d390a964cca2cac502c0b645996bce8d0
NicolasFOESSEL <nicofoessel91740@gmail.com>
2021-12-06 11:15:46 +0100

    Update Post.php

--- src/Entity/Post.php
+++ src/Entity/Post.php
@@ -4,25 +4,5 @@ namespace App\Entity;
 
 class Post
 {
-    private int $id;
-    private \DateTime $date;
-    private string $title;
-    private string $content;
-    private int $authorId;
 
-    /**
-     * @return string
-     */
-    public function getTitle(): string
-    {
-        return $this->title;
-    }
-
-    /**
-     * @param string $title
-     */
-    public function setTitle(string $title): void
-    {
-        $this->title = $title;
-    }
 }
