6c3289ce95c793fb5b69ac74a739b999c941d6d9
NicolasFOESSEL <nicofoessel91740@gmail.com>
2021-12-06 11:15:40 +0100

    Update ErrorController.php

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -4,12 +4,5 @@ namespace App\Controller;
 
 class ErrorController extends BaseController
 {
-    public function executeError404()
-    {
-        $this->render(
-            '404.php',
-            [],
-            'Mauvaise Route'
-        );
-    }
+
 }
