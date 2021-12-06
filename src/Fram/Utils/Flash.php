1c4b0e28ac405257f58c49bfa0037b420f8e2de5
NicolasFOESSEL <nicofoessel91740@gmail.com>
2021-12-06 11:15:48 +0100

    Update Flash.php

--- src/Fram/Utils/Flash.php
+++ src/Fram/Utils/Flash.php
@@ -4,23 +4,6 @@ namespace App\Fram\Utils;
 
 class Flash
 {
-    public static function setFlash(string $channel, string $message): void
-    {
-        $_SESSION[$channel] = htmlspecialchars($message);
-    }
 
-    public static function hasFlash(string $channel): bool
-    {
-        return isset($_SESSION[$channel]);
-    }
 
-    public static function getFlash(string $channel)
-    {
-        if (self::hasFlash($channel)) {
-            $message = $_SESSION[$channel];
-            unset($_SESSION[$channel]);
-        }
-
-        return $message;
-    }
 }
