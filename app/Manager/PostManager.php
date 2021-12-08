c2b8b901ac7ae69f562d2ee7ea23a11fffde5a7b
NicolasFOESSEL <nicofoessel91740@gmail.com>
2021-12-08 11:14:03 +0100

    Update PostManager.php

--- app/Manager/PostManager.php
+++ app/Manager/PostManager.php
@@ -8,4 +8,45 @@ class PostManager extends BaseManager
 {
 
 
+    /**
+     * @return Post[]
+     */
+    public function getAllPosts(): array
+    {
+
+        return [];
+    }
+
+    public function getPostById(int $id): Post
+    {
+        // TODO - Posts by Id
+    }
+
+    /**
+     * @param Post $post
+     * @return Post|bool
+     */
+    public function createPost(Post $post)
+    {
+        // TODO - create post
+        return true;
+    }
+
+    /**
+     * @param Post $post
+     * @return Post|bool
+     */
+    public function updatePost(Post $post)
+    {
+        // TODO - getPostById($post->getId())
+    }
+
+    /**
+     * @param int $id
+     * @return bool
+     */
+    public function deletePostById(int $id): bool
+    {
+        // TODO - Delete post
+    }
 }
