cdad2e4e875a3f887f35bf2274d6d87a6187cc18
Doustalet Chloé <cdoustalet@gmail.com>
2021-12-09 09:47:59 +0100

    teacher_update_homepage_and_classPDO

--- app/Fram/Factories/PDOFactory.php
+++ app/Fram/Factories/PDOFactory.php
@@ -8,11 +8,11 @@ class PDOFactory
     private static string $username ='root';
     private static string $password = 'example';
 
-    public static function getMysqlConnection(): PDO{
+    public static function getMysqlConnection(): \PDO{
         try{
-            return new PDO(self::$dsn, self::$username, self::$password);
+            return new \PDO(self::$dsn, self::$username, self::$password);
         }
-        catch(PDOException $e){
+        catch(\PDOException $e){
             return "Connection failed: " . $e->getMessage();
         };
     }
