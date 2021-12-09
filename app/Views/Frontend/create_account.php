b84399d14223ec9aa6113a3006133533a35d149a
Doustalet Chloé <cdoustalet@gmail.com>
2021-12-09 16:39:28 +0100

    Add views and routes
    Add the Login, Subscribe and Create Article Views.
    Add the routes for these views

--- app/Views/Frontend/create_account.php
+++ app/Views/Frontend/create_account.php
@@ -1,1 +1,42 @@
-<h1>Je suis la page pour me créer un compte</h1>
+<div class="h-screen bg-100 flex justify-center items-center">
+    <div class="lg:w-2/5 md:w-1/2 w-2/3">
+        <form class="bg-white p-10 rounded-lg shadow-lg min-w-full">
+            <h1 class="text-center text-2xl mb-6 text-gray-600 font-bold font-sans">Créer un compte</h1>
+
+            <!-- Nom -->
+            <div>
+                <label class="text-gray-800 font-semibold block my-3 text-md" for="username">Nom</label>
+                <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="nom" id="nom" placeholder="Nom" />
+            </div>
+
+            <!-- Prénom -->
+            <div>
+                <label class="text-gray-800 font-semibold block my-3 text-md" for="username">Prénom</label>
+                <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="prénom" id="prénom" placeholder="Prénom" />
+            </div>
+
+            <!-- E-mail -->
+            <div>
+                <label class="text-gray-800 font-semibold block my-3 text-md" for="email">E-mail</label>
+                <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="email" id="email" placeholder="@email" />
+            </div>
+
+            <!-- Mot de passe -->
+            <div>
+                <label class="text-gray-800 font-semibold block my-3 text-md" for="password">Mot de passe</label>
+                <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="password" id="password" placeholder="Mot de passe" />
+            </div>
+
+            <!-- Confirmation de mot de passe -> A FAIRE SI TEMPS -->
+            <!-- <div>
+                <label class="text-gray-800 font-semibold block my-3 text-md" for="confirm">Confirm password</label>
+                <input class="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none" type="text" name="confirm" id="confirm" placeholder="confirm password" />
+            </div> -->
+            <!-- Boutton -->
+            <!-- Submit -->
+            <button type="submit" class="w-full mt-6 bg-purple-600 rounded-lg px-4 py-2 text-lg text-white tracking-wide font-semibold font-sans">Créer un compte</button>
+            <!-- Redirection page de connexion -->
+            <button type="submit" class="w-full mt-6 mb-3 bg-purple-100 rounded-lg px-4 py-2 text-lg text-gray-800 tracking-wide font-semibold font-sans">se connecter</button>
+        </form>
+    </div>
+</div>
--- app/Views/Frontend/create_article.php
+++ app/Views/Frontend/create_article.php
@@ -1,1 +1,40 @@
-<h1>Je suis la page pour créer des articles</h1>
+<div class="w-full">
+    <div class="bg-gradient-to-b from-purple-800 to-purple-600 h-96 z-0"></div>
+    <div class="max-w-5xl mx-auto px-6 sm:px-6 lg:px-8 mb-12">
+
+        <div class="bg-white w-full shadow rounded p-8 sm:p-12 -mt-72">
+            <p class="text-3xl font-bold leading-7 text-center">Nouveau post</p>
+            <form action="" method="post">
+                <div class="md:flex items-center mt-12">
+                    <div class="w-full md:w-1/2 flex flex-col">
+                        <label class="font-semibold leading-none">Nom</label>
+                        <input type="text" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-purple-700 mt-4 bg-gray-100 border rounded border-gray-200" placeholder="TODO" />
+                    </div>
+                    <div class="w-full md:w-1/2 flex flex-col md:ml-6 md:mt-0 mt-4">
+                        <label class="font-semibold leading-none">Prénom</label>
+                        <input type="email" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-purple-700 mt-4 bg-gray-100 border rounded border-gray-200" placeholder="TODO"/>
+                    </div>
+                </div>
+                <div class="md:flex items-center mt-8">
+                    <div class="w-full flex flex-col">
+                        <label for="post" class="font-semibold leading-none">Post</label>
+                        <input id="post" type="text" class="leading-none text-gray-900 p-3 focus:outline-none focus:border-purple-700 mt-4 bg-gray-100 border rounded border-gray-200"/>
+                    </div>
+
+                </div>
+                <div>
+                    <div class="w-full flex flex-col mt-8">
+                        <label for="message" class="font-semibold leading-none">Message</label>
+                        <textarea id="message" type="text" class="h-40 text-base leading-none text-gray-900 p-3 focus:oultine-none focus:border-purple-700 mt-4 bg-gray-100 border rounded border-gray-200"></textarea>
+                    </div>
+                </div>
+                <div class="flex items-center justify-center w-full">
+                    <button class="mt-9 font-semibold leading-none text-white py-4 px-10 bg-purple-700 rounded hover:bg-purple-600 focus:ring-2 focus:ring-offset-2 focus:ring-blue-700 focus:outline-none">
+                        POSTER
+                    </button>
+                </div>
+            </form>
+        </div>
+    </div>
+
+</div>
