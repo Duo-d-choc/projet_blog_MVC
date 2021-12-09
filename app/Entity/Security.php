da219329eadbe8be7b7fc94d223c5cc753b3a064
NicolasFOESSEL <nicofoessel91740@gmail.com>
2021-12-09 17:00:50 +0100

    Create Security.php

---
+++ app/Entity/Security.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Entity;
+//use App\Fram\Factories\PDOFactory;
+
+class Security
+{
+    private string $email;
+    private string $password;
+    private string $statut;
+
+
+    /**
+     * @return string
+     */
+    public function getEmail(): string
+    {
+        return $this->email;
+    }
+
+    /**
+     * @param string $email
+     */
+    public function setEmail(string $email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    /**
+     * @param string $password
+     */
+    public function setPassword(string $password): void
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * @return string
+     */
+    public function getStatut(): string
+    {
+        return $this->statut;
+    }
+
+    /**
+     * @param string $statut
+     */
+    public function setStatut(string $statut): void
+    {
+        $this->statut = $statut;
+    }
+}
