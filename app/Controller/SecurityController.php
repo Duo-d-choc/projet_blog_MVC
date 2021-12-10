3fb77fc5cff21b54a93e011f4bf03f7984169722
NicolasFOESSEL <nicofoessel91740@gmail.com>
2021-12-10 11:23:56 +0100

    Update SecurityController.php

--- app/Controller/SecurityController.php
+++ app/Controller/SecurityController.php
@@ -4,39 +4,36 @@ namespace App\Controller;
 
 use App\Entity\Users;
 use App\Fram\Utils\Flash;
-use App\Manager\PostManager;
+use App\Entity\Security;
+use App\Manager\SecurityManager;
 use App\Fram\Factories\PDOFactory;
+
 
 
 class SecurityController extends BaseController
 {
     public function executeLogin (){
 
-        $manager = new PostManager(PDOFactory::getMysqlConnection());
 
-        //ligne a mettre pour connexion
-        $compte = $manager->getAllPosts($number);
+        $_SESSION['token'] = SecurityManager::connexion(PDOFactory::getMysqlConnection());
+
+        if ($_SESSION['token']){
 
 
-        if ($compte){
-            return $_SESSION['connect'] = true;
+            $this->render('login.php', [], 'Connexion');
         }
-        return false;
 
 
-
-
-        $this->render('login.php', [], 'Connexion');
 
 
     }
 
     public function executeCreateAccount (){
 
-        $manager = new PostManager(PDOFactory::getMysqlConnection());
+        SecurityManager::createAccount($_POST);
+        var_dump($_POST);
 
-        //ligne a mettre pour création de compte
-        $articles = $manager->getAllPosts($number);
+
 
 
         $this->render('create_account.php', [], 'Créer un compte');
